94ff795516036c4723fce6483b05a45191de1a8e
Ljudmila Dolgikh <dolgikg20ljudmila@yandex.ru>
2025-08-08 21:20:27 +0300

    URL для проекта

---
+++ url.py
@@ -0,0 +1,5 @@
+class URLCollection:
+    SCOOTER_HOME_PAGE = 'https://qa-scooter.praktikum-services.ru/'
+    YANDEX_HOME_PAGE = 'https://dzen.ru/?yredirect=true'
+    SCOOTER_ORDER_PAGE = 'https://qa-scooter.praktikum-services.ru/order'
+
